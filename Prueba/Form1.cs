33eb34f0fc33d898c636319b1048686fd14177c3
Railton Cardoso <railton.cardoso@hotmail.com>
2017-10-17 17:31:34 +0200

    Métodos para añadir pintura-D Alberto

--- Prueba/Form1.cs
+++ Prueba/Form1.cs
@@ -16,5 +16,82 @@ namespace Prueba
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAnyadir_Click(object sender, EventArgs e)
+        {
+            if (HayError())
+                return;
+
+            double precio = Convert.ToDouble(txtPrecio.Text);
+
+            // si no hay error añado la pintura a la lista y al combo
+            listaPinturas.Add(new Pintura(nuevoColor, txtNombreColor.Text, precio));
+            cbColor.Items.Add(new Pintura(nuevoColor, txtNombreColor.Text, precio));
+
+            MessageBox.Show("La pintura ha sido añadida a la lista", "Operación OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        bool HayError()
+        {
+            bool error = false;
+            string mensajeError = "";
+            double precio;
+            errorProvider1.Clear();
+
+            if (txtNombreColor.Text.Length < 1)
+            {
+                error = true;
+                mensajeError = "Debe escribir el nombre del color\n";
+
+            }
+            else
+            {
+                foreach (Pintura p in listaPinturas)
+                    if (txtNombreColor.Text == p.Nombre)
+                    {
+                        error = true;
+                        mensajeError += "Ya existe un color con dicho nombre\n";
+                        break;
+                    }
+            }
+
+            if (txtPrecio.Text.Length < 1)
+            {
+                error = true;
+                mensajeError += "Debe introducir el precio\n";
+            }
+            else if (!Double.TryParse(txtPrecio.Text, out precio))
+            {
+                error = true;
+                mensajeError += "No ha indicado un precio válido\n";
+            }
+
+            if (nuevoColor == this.BackColor)
+            {
+                error = true;
+                mensajeError += "Debe elegir un color\n";
+            }
+            else
+            {
+                foreach (Pintura p in listaPinturas)
+                    if (nuevoColor == p._Color)
+                    {
+                        error = true;
+                        mensajeError += "Ya existe el color elegido\n";
+                        break;
+                    }
+            }
+
+            if (error)
+                MessageBox.Show(mensajeError, "Hay error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            return error;
+        }
+
     }
 }
